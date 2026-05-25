5bae4212cefa686e7e3e2bb757dc4e0af5529637
Rocio Altamirano <rochyaltamirano@gmail.com>
2026-05-24 22:48:37 -0300

    Tests cases de comprar un producto con usuario logueado y sin loguearse

---
+++ cypress/e2e/cart/checkout.cy.js
@@ -0,0 +1,68 @@
+import * as LoginPage from '../../support/selectores/login-page';
+import * as CheckoutPage from '../../support/selectores/checkout-page';
+const { faker } = require('@faker-js/faker');
+describe('Comprar productos', () => {
+   // Este test verifica que un producto se pueda agregar al carrito correctamente
+    it('Comprar un producto al carrito desde el detalle sin loguearse', () => {
+        const password = faker.internet.password();
+        
+        cy.visit('/index.php?route=product/product&product_id=58');
+        cy.get(CheckoutPage.BUY_BUTTON).contains('Buy now').click();
+
+        cy.get(CheckoutPage.FIRST_NAME_INPUT).type(faker.person.firstName());
+        cy.get(CheckoutPage.LAST_NAME_INPUT).type(faker.person.lastName());
+        cy.get(CheckoutPage.EMAIL_INPUT).type(faker.internet.email());
+        cy.get(CheckoutPage.TELEPHONE_INPUT).type(faker.phone.number('1122223333'));
+        cy.get(CheckoutPage.PASSWORD_INPUT).type(password);
+        cy.get(CheckoutPage.CONFIRM_PASSWORD_INPUT).type(password);
+
+        cy.get(CheckoutPage.COMPANY_INPUT).type(faker.company.name());
+        cy.get(CheckoutPage.ADDRESS_1_INPUT).type(faker.location.streetAddress());
+        cy.get(CheckoutPage.CITY_INPUT).type(faker.location.city());
+        cy.get(CheckoutPage.POSTCODE_INPUT).type(faker.location.zipCode('#####'));
+        cy.get(CheckoutPage.COUNTRY_SELECT).select('Argentina');
+        cy.get(CheckoutPage.ZONE_SELECT).select('Buenos Aires');
+
+        cy.get(CheckoutPage.COMMENT_TEXTAREA).type('Por favor, entregar entre las 9am y 5pm.');
+        cy.get(CheckoutPage.AGREE_TyC_CHECKBOX).click();
+        cy.get(CheckoutPage.AGREE_POLICY_CHECKBOX).click();
+        cy.get(CheckoutPage.SUBMIT_BUTTON).contains('Continue').click();
+
+        //COnfirmar orden
+        cy.contains('Confirm Order').should('be.visible');
+
+        cy.get(CheckoutPage.CONFIRM_ORDER_BUTTON).contains('Confirm Order').click();
+
+        // Validar que se muestre el mensaje de éxito
+        cy.contains('Your order has been placed!').should('be.visible');     
+    });
+
+    it('Comprar un producto al carrito desde el detalle logueado', () => {
+        const email = 'ralta@yopmail.com';
+        const password = '1q2w3e4r5t';
+
+        cy.visit('/index.php?route=account/login');
+      
+        cy.get(LoginPage.EMAIL).type(email);
+        cy.get(LoginPage.PASSWORD).type(password);
+        cy.get(LoginPage.LOGIN_BUTTON).click();
+
+        cy.visit('/index.php?route=product/product&product_id=58');
+
+        cy.get(CheckoutPage.BUY_BUTTON).contains('Buy now').click();
+
+        cy.get(CheckoutPage.COMMENT_TEXTAREA).type('Por favor, entregar entre las 9am y 5pm.');
+        cy.get(CheckoutPage.AGREE_TyC_CHECKBOX).click();
+        cy.get(CheckoutPage.SUBMIT_BUTTON).contains('Continue').click();
+
+        //Confirmar orden
+        cy.contains('Confirm Order').should('be.visible');
+
+        cy.get(CheckoutPage.CONFIRM_ORDER_BUTTON).contains('Confirm Order').click();
+
+        // Validar que se muestre el mensaje de éxito
+        cy.contains('Your order has been placed!').should('be.visible');  
+
+    });
+
+})
---
+++ cypress/support/selectores/checkout-page.js
@@ -0,0 +1,21 @@
+export const BUY_BUTTON = '[type="button"]';
+
+export const FIRST_NAME_INPUT = '[name="firstname"]';
+export const LAST_NAME_INPUT = '[name="lastname"]';
+export const EMAIL_INPUT = '[name="email"]';
+export const TELEPHONE_INPUT = '[name="telephone"]';
+export const PASSWORD_INPUT = '[name="password"]';
+export const CONFIRM_PASSWORD_INPUT = '[name="confirm"]';
+export const COMPANY_INPUT = '[name="company"]';
+export const ADDRESS_1_INPUT = '[name="address_1"]';
+export const CITY_INPUT = '[name="city"]';
+export const POSTCODE_INPUT = '[name="postcode"]';
+export const COUNTRY_SELECT = '[name="country_id"]';
+export const ZONE_SELECT = '[name="zone_id"]';
+
+export const AGREE_TyC_CHECKBOX = '[for="input-agree"]';
+export const AGREE_POLICY_CHECKBOX = '[for="input-account-agree"]';
+
+export const SUBMIT_BUTTON = '[type="submit"]';
+export const COMMENT_TEXTAREA = '#input-comment';
+export const CONFIRM_ORDER_BUTTON = '#button-confirm';  
